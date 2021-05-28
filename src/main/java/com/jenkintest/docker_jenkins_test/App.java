3d7e2d51ab3c75e777b5f89d7d1c704bd781aa5a
Shrenuj Kabra <shrenuj.kabra@centurylink.com>
2021-05-28 12:05:31 +0530

    Initial commit

---
+++ src/main/java/com/jenkintest/docker_jenkins_test/App.java
@@ -0,0 +1,13 @@
+package com.jenkintest.docker_jenkins_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
