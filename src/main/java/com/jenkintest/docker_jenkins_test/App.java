af39b34a91a435f56d601fba88846067923fb916
Shrenuj Kabra <shrenuj.kabra@centurylink.com>
2021-06-02 12:03:38 +0530

    updated App.java

--- src/main/java/com/jenkintest/docker_jenkins_test/App.java
+++ src/main/java/com/jenkintest/docker_jenkins_test/App.java
@@ -1,9 +1,6 @@
 package com.jenkintest.docker_jenkins_test;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
--- src/main/java/com/jenkintest/docker_jenkins_test/Test.java
+++
@@ -1,8 +0,0 @@
-package com.jenkintest.docker_jenkins_test;
-
-public class Test {
-
-	public static void main(String args[]) {
-		System.out.println("Hi Shrenuj");
-	}
-}
